5c85b9b07712a9bbab401a3a95ce14f200c47d9b
avnoor-ludhar <ludhar1@uwindsor.ca>
2024-05-07 12:42:20 -0400

    added button and recorded speech

--- Front-End/src/Pages/Home.tsx
+++ Front-End/src/Pages/Home.tsx
@@ -1,14 +1,27 @@
 import { useAppSelector } from "@/redux/store";
 import { useEffect, useState } from "react";
 import axios, {AxiosError, AxiosResponse} from "axios";
+import { FaMicrophoneAlt } from "react-icons/fa";
+import { useReactMediaRecorder, StatusMessages } from "react-media-recorder";
 
 type responseData = {
     success: string
 }
 
+type mediaHookType = {
+    status: StatusMessages,
+    startRecording: () => void,
+    stopRecording: () => void,
+    mediaBlobUrl: undefined | string
+}
+
+
+//need to use clearBlobUrl function from the hook when we get data back from the whisper API
+
 export default function Home(): JSX.Element{
     const user = useAppSelector(state=>state.user.user);
     const [data, setData] = useState<string | null>();
+    const {status, startRecording, stopRecording, mediaBlobUrl}: mediaHookType = useReactMediaRecorder({audio: true})
     
     useEffect(()=>{
         const someFunction = async ()=>{
@@ -37,9 +50,23 @@ export default function Home(): JSX.Element{
         
         
     }, []);
+
     
-    return (<div>
-            {data}
-            {user?.email}
+    const handleRecord = () =>{
+        if(status === 'recording'){
+            stopRecording();
+        } else{
+            startRecording();
+        }
+    }
+    
+    return (
+        <div>
+            <p>{data}</p>
+            <p>{user?.email}</p>
+            <div className="flex flex-col items-center">
+                <button className="mt-20 scale-[3] bg-red-600 p-1 rounded-full hover:opacity-80" onClick={handleRecord}><FaMicrophoneAlt /></button>
+                <audio src={mediaBlobUrl} controls autoPlay loop className="mt-10"/>
+            </div>
         </div>)
 }
