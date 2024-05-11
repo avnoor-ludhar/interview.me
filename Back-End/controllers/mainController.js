762d7886a9ff07e2ada27750a0abf189c69a4027
avnoor-ludhar <ludhar1@uwindsor.ca>
2024-05-11 17:26:01 -0400

    Implemented the transcription to the frontend using DeepGram SDK and using web hooks. Note need to still change so that on page load we connect to the software and keep the connection open but turn on and off the mic or fix the error on the backend

--- Back-End/controllers/mainController.js
+++ Back-End/controllers/mainController.js
@@ -1,6 +1,24 @@
+import { createClient } from "@deepgram/sdk";
+
+const deepgram = createClient(process.env.DEEPGRAM_APIKEY)
 
 const dataFunc = async (req, res)=>{
     res.send({success: 'success'});
 }
 
-export {dataFunc};
+const transcribeURL = async (req, res)=>{
+    const {url} = req.body;
+    const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
+        {
+          url: url
+        },
+        {
+          model: "nova-2",
+        }
+      );
+    console.log(error);
+
+    res.send({transcription: result}).status(200);
+}
+
+export {dataFunc, transcribeURL};
