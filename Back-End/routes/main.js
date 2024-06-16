80083175d882f85d8fbe8ba5658bf99e85dd619f
avnoor-ludhar <ludhar1@uwindsor.ca>
2024-06-16 15:50:17 -0400

    finished text to speech via an audio queue however it has some errors by skipping some chunks and misordering them

--- Back-End/routes/main.js
+++ Back-End/routes/main.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import requireAuth from '../middleware/requireAuth.js';
-import { dataFunc } from '../controllers/mainController.js';
-import { transcribeURL } from '../controllers/mainController.js';
+import { dataFunc, textToSpeechDeepgram } from '../controllers/mainController.js';
 
 const router = express.Router();
 
@@ -9,6 +8,6 @@ router.use(requireAuth);
 
 router.get('/', dataFunc);
 
-router.post('/', transcribeURL);
+router.post('/tts', textToSpeechDeepgram);
 
 export default router;
--- Front-End/src/Pages/Home.tsx
+++ Front-End/src/Pages/Home.tsx
@@ -1,7 +1,20 @@
 import { useAppSelector } from "@/redux/store";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { start } from "@/utils/microphone";
 import { FaMicrophoneAlt } from "react-icons/fa";
+import axios, { AxiosResponse } from "axios";
+import { useNavigate } from "react-router-dom";
+import { models } from "@/assets/models";
+
+type speaker = {
+    speaker: string,
+    text: string
+};
+
+type bodyTTS = {
+    text: string,
+    model: string
+}
 
 const isJSON = (str: string) =>{
     try {
@@ -12,11 +25,6 @@ const isJSON = (str: string) =>{
     }
 }
 
-type speaker = {
-    speaker: string,
-    text: string
-};
-
 export default function Home(): JSX.Element{
     const user = useAppSelector(state=>state.user.user);
     const [currentTranscription, setCurrentTranscription] = useState<string>("");
@@ -25,21 +33,56 @@ export default function Home(): JSX.Element{
     const [isRecording, setIsRecording] = useState<boolean>(false);
     const socketRef = useRef<null | WebSocket>(null);
     const microphoneRef = useRef<MediaRecorder | null>(null);
+    const [audioBlob, setAudioBlob] = useState<string | null>(null);
+    const navigate = useNavigate();
+    const [audioQueue, setAudioQueue] = useState<string[]>([]);
+    const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(null);
 
-    useEffect(() => {
-        console.log("ChatLog updated:", chatLog);
-    }, [chatLog]);
+    if(!user){
+        navigate('/');
+    }
 
+    // Function to add audio to the queue
+    const addToQueue = (audioUrl: string) => {
+        setAudioQueue((prevQueue) => [...prevQueue, audioUrl]);
+    };
+
+    // Function to play the next audio in the queue
+    const playNextAudio = useCallback(() => {
+        if (audioQueue.length > 0) {
+            //takes the first audio sample in the queue
+            const nextAudioUrl = audioQueue[0];
+            //makes an HTML audio object out of it to play the sound
+            const audio = new Audio(nextAudioUrl);
+            
+            // this is an event listener to modify the queu on the ending of an audio sample
+            audio.addEventListener('ended', () => {
+                setAudioQueue((prevQueue) => prevQueue.slice(1));
+                setCurrentAudio(null);
+            });
+
+            // Start playback
+            setCurrentAudio(audio);
+            audio.play();
+        }
+    }, [audioQueue]);
+
+    // Effect to play the next audio when the queue changes
+    // we add the playNextAudio to our dependency array to ensure 
+    // that the version of the function is the re-rendered function
+    // since useCallback memoizes the function
     useEffect(() => {
-        console.log("CurrentSpeaker updated:", currentSpeaker);
-    }, [currentSpeaker]);
+        //checks if the currentAudio is null and we have some audio in our queue
+        if (!currentAudio && audioQueue.length > 0) {
+            playNextAudio();
+        }
+    }, [audioQueue, currentAudio, playNextAudio]);
 
     const activateConnection = async ()=>{
         socketRef.current = new WebSocket(import.meta.env.VITE_WEBSOCKET_URL);
        
         socketRef.current.addEventListener('open', async () =>{
             console.log("WebSocket connection opened");
-            // Ensure 'this' is the WebSocket instance
             if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
                 socketRef.current.send(JSON.stringify({ type: 'start_deepgram_session' }));
             } else {
@@ -67,6 +110,7 @@ export default function Home(): JSX.Element{
 
     const handleWebSocketMessage = (data: any) => {
         if (data.chunk) {
+            convertTextToSpeech(data.chunk);
             updateStateWithChunk(data.chunk);
         } else {
             const transcriptionFromBackEnd = data.transcript;
@@ -76,11 +120,38 @@ export default function Home(): JSX.Element{
         }
     }
 
+    const convertTextToSpeech = async (text: string) => {
+        try {
+            const url:string = `${import.meta.env.VITE_REACT_APP_API_URL}/api/interview/tts`;
+            const body: bodyTTS = {text: text, model: models[1].model};
+
+            if(user){
+                const response: AxiosResponse = await axios.post(url, body, {
+                    headers: {
+                      'Authorization': `Bearer ${user.token}`
+                    },
+                    responseType: 'arraybuffer'
+                });
+
+                const audioUrl = URL.createObjectURL(new Blob([response.data], { type: "audio/wav" }));
+                addToQueue(audioUrl);
+                //setAudioBlob(audioUrl);
+            }else{
+                throw Error('User is not signed in.');
+            }
+        } catch (error) {
+            console.error('Error converting text to speech:', error);
+        }
+      };
+
     const updateStateWithChunk = (chunk: string) => {
         setCurrentSpeaker((prev) => {
-            const newSpeaker = prev.speaker === "User"
-                ? { speaker: "Gemini", text: chunk }
-                : { ...prev, text: prev.text + " " + chunk };
+            let newSpeaker = { ...prev, text: prev.text + " " + chunk };
+            if(prev.speaker === "User"){
+                newSpeaker = { speaker: "Gemini", text: chunk };
+            } else if(chunk[0] === "." || chunk[0] === "," || chunk[0] === "'" || chunk[0] === ";" || chunk[0] == ":" || chunk[0] === "?" || chunk[0] === "!" || prev.text[prev.text.length - 1] == "'"){
+                newSpeaker = {...prev, text: prev.text + chunk}
+            }
 
             if (prev.speaker === "User") {
                 setChatLog((log) => {
@@ -152,7 +223,7 @@ export default function Home(): JSX.Element{
                     {geminiMessages.map((transcript, i)=>(<p key={i}>{transcript}</p>))}
                 </div>
             </div> */}
-            
+            {audioBlob && <audio controls src={audioBlob} autoPlay />}
             <p>{user?.email}</p>
             <div className="flex flex-col items-center">
                 <button className="mt-20 scale-[3] bg-red-600 p-1 rounded-full hover:opacity-80" onClick={handleRecord}><FaMicrophoneAlt /></button>
