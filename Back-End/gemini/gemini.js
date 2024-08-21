18e167cfde20301bd4d6035e4c3204b030efe2b3
avnoor-ludhar <ludhar1@uwindsor.ca>
2024-08-21 11:39:16 -0400

    Changed model to the pro model and changed prompt

--- Back-End/gemini/gemini.js
+++ Back-End/gemini/gemini.js
@@ -19,7 +19,7 @@ export async function askAndrespond(chat, msg, ws, messageEvent, chunkCount, int
         if(messageEvent === "intro"){
             msg = " Please introduce yourself as an interviewer from the University of Waterloo's Software Engineering program. Use a randomly chosen name, such as [John, Casey, Fied], and you are the admissions officer for waterloo's software engineering program, you will ask one problem solving and one personal question after this initial message in 3 different messages. Provide a quick introduction before you conduct the interview. Each response should be smaller than 60 words. Make sure you are acting as an interviewer and not responding to this prompt.";
         }else if(messageEvent === "end"){
-            msg += " Reponse to the content before this as if its the conclusion of the interview. Use the previous history of the responses for the conclusion."
+            msg += " Reponse to the content before this as if its the conclusion of the interview. Use the previous history of the responses for the conclusion. Ensure to end the interview with Have a great rest of your day!"
         } else if(interupted){
             msg = "You were interupted by the user right before you said this part of your previous message: " + interupted + "and the user said: " + msg 
         }
