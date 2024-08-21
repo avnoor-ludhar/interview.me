1f8f6a983786299d3d9c0e6215c3dcb8bd8a5bf9
avnoor-ludhar <ludhar1@uwindsor.ca>
2024-08-21 10:46:41 -0400

    Merged priority-queue branch to main

--- Back-End/gemini/gemini.js
+++ Back-End/gemini/gemini.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 //creates a connection to the Gemini API
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_APIKEY);
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+export const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
 //sets up a chat so that the model can rememebr history of the conversation
 export const chat = model.startChat({
     history: [],
