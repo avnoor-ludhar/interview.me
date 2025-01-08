f12ddc1de32b71662c3d8cc1b3a8a138c85b6855
avnoor-ludhar <ludhar1@uwindsor.ca>
2025-01-07 22:21:40 -0500

    Added new style to the dashboard with the chat log on the right in large screens and a flex-col on below large screens. Still need to fetch the chatlog and necessary information from the backend and display it in the dashboard

--- Front-End/src/Pages/Home.tsx
+++ Front-End/src/Pages/Home.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { TrendingUp } from "lucide-react";
 import { Graph } from "../components/Graph";
 import { Area } from "../components/Area";
+import Chat from "@/components/Chat";
 
 
 
@@ -17,73 +18,67 @@ const Home = () => {
     }
 
     return (
-        <div className="h-screen flex justify-between items-start font-poppins">
-            {/*<div className=" w-72 h-full p-4 bg-darkGray border-r-2 border-gray-800 align-items">
-                <div className="">
-                    <h1 className="font-semibold mt-5"> Interview.me</h1>
-                </div>
-                <div className="">
-                    <Button variant = "ghost" className="text-lg hidden md:flex ">Log Out</Button>
-                </div>
-                <div className="">
-                    <h1 className="font-semibold mt-5"> Interview.me</h1>
-                </div>
-                <div className="">
-                    <h1 className="font-semibold mt-5"> Interview.me</h1>
-                </div>
-            </div> We first need to see if we even have anything to put on here, you can uncomment it and see what it is
-            */}
+        <div className="flex justify-between items-start font-poppins">
             <div className="w-full h-full p-4 flex flex-col bg-night">
                 {/* Top Section */}
-                <div className="w-full text-white mb-6 text-left pl-40 flex justify-between items-center p-4">
+                <div className="w-full text-white mb-6 text-left pl-20 flex justify-between items-center p-4">
                     <h2 className="text-2xl font-bold">Welcome Back {user?.email} 👋 </h2>
-                    <Button className="shadow-2xl shadow-indigo-500/50 mr-12" variant="outline" onClick={() => navigate("/intake")}>
-                        Begin Interview
-                    </Button>
+                    <div className="flex flex-row justify-start gap-4">
+                        <Button className="shadow-2xl shadow-indigo-500/50" variant="outline" onClick={() => navigate("/intake")}>
+                            Begin Interview
+                        </Button>
+                        <Button className="shadow-2xl shadow-indigo-500/50 mr-12" variant="outline" onClick={() => navigate("/intake")}>
+                            Log out
+                        </Button>
+                    </div>
+                    
                 </div>
 
                 {/* Cards Section */}
-                <div className="flex flex-wrap gap-16 justify-center mt-8">
-                    {/* Card 1 */}
-                    <div className="w-full md:w-1/3 lg:w-1/4 p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
-                        <h3 className="font-bold text-white">Recent Performance</h3>
-                        <p className="text-white text-sm">Scored from 1-10</p>
-                        <Graph />
-                    </div>
-                    {/* Card 2 */}
-                    <div className="w-full md:w-1/3 lg:w-1/4 p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
-                        <h3 className="font-bold text-white">Average Performance</h3>
-                        <p className="text-white text-sm">Scale from 1-10</p>
-                        <Area />
-                    </div>
-                    {/* Card 3 */}
-                    <div className="w-full md:w-1/3 lg:w-1/4 p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
-                        <h3 className="font-bold text-white">Weak Points</h3>
-                        <p className="text-white text-sm">These are things you can improve upon</p>
-                        <ul className="list-disc list-inside text-white pt-8 text-left ml-12 space-y-4">
-                            <li>Weakness 1: Confidence</li>
-                            <li>Weakness 2: Fluency</li>
-                            <li>Weakness 3: Speed</li>
-                        </ul>
-                    </div>
-                    {/* Card 4 */}
-                    <div className="w-full md:w-1/3 lg:w-1/4 p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
-                        <h3 className="font-bold text-white">Strong Points</h3>
-                        <p className="text-white text-sm">These are things you do well!</p>
-                        <ul className="list-disc list-inside text-white pt-8 text-left ml-12 space-y-4">
-                            <li>Strength 1: Confidence</li>
-                            <li>Strength 2: Fluency</li>
-                            <li>Strength 3: Speed</li>
-                        </ul>
+                <div className="flex lg:flex-row flex-col-reverse gap-8 w-full items-center justify-between mt-4">
+                    <div className="flex flex-wrap flex-row justify-evenly gap-10 w-3/4 mb-10">
+                        {/* Card 1 */}
+                        <div className="w-full lg:w-[45%] max-w-[400px] p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
+                            <h3 className="font-bold text-white">Recent Performance</h3>
+                            <p className="text-white text-sm">Scored from 1-10</p>
+                            <Graph />
+                        </div>
+                        {/* Card 2 */}
+                        <div className="w-full lg:w-[45%] max-w-[400px] p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
+                            <h3 className="font-bold text-white">Average Performance</h3>
+                            <p className="text-white text-sm">Scale from 1-10</p>
+                            <Area />
+                        </div>
+                        {/* Card 3 */}
+                        <div className="w-full lg:w-[45%] max-w-[400px] p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
+                            <h3 className="font-bold text-white">Weak Points</h3>
+                            <p className="text-white text-sm">These are things you can improve upon</p>
+                            <ul className="list-disc list-inside text-white pt-8 text-left ml-4 space-y-4">
+                                <li>Weakness 1: Confidence</li>
+                                <li>Weakness 2: Fluency</li>
+                                <li>Weakness 3: Speed</li>
+                            </ul>
+                        </div>
+                        {/* Card 4 */}
+                        <div className="w-full lg:w-[45%] max-w-[400px] p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2">
+                            <h3 className="font-bold text-white">Strong Points</h3>
+                            <p className="text-white text-sm">These are things you do well!</p>
+                            <ul className="list-disc list-inside text-white pt-8 text-left ml-4 space-y-4">
+                                <li>Strength 1: Confidence</li>
+                                <li>Strength 2: Fluency</li>
+                                <li>Strength 3: Speed</li>
+                            </ul>
+                        </div>
                     </div>
                     {/* Card 5 */}
-                    <div className="w-full md:w-1/3 lg:w-1/4 p-4 bg-darkGray rounded-3xl drop-shadow-2xl h-[300px] border-2 mr-24 mb-5">
-                        <h3 className="font-bold text-white">Data Point 5</h3>
-                        <p className="text-white text-sm">Description or value here.</p>
+                    <div className="w-3/4 lg:w-1/3 p-4 bg-darkGray rounded-3xl drop-shadow-2xl border-2 h-fit max-h-[500px]">
+                        <h3 className="font-bold text-white">Interview Chat Log</h3>
+                        <p className="text-white text-sm">View your most recent chat log!</p>
+                        {/* <Chat /> */}
                     </div>
-                    {/* Card 6 */}
-
                 </div>
+                
+                
             </div>
         </div>
 
