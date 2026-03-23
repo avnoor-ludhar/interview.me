76eef56279b81841157657833efbabb49c7c042c
Sehajdeep Dhillon <dhillonsehaj63@gmail.com>
2026-03-23 11:23:13 -0400

    colab set up

--- Ml/main.py
+++ Ml/main.py
@@ -1,12 +1,1 @@
-{
- "cells": [],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import requests
